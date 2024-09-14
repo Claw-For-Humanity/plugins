8e4c83c86d3d015ba357f3c326211655e15d7c1b
Eric Kang <97197222+erickdhdock@users.noreply.github.com>
2024-09-14 00:13:59 -0700

    Update inference.py

--- sam2/inference.py
+++ sam2/inference.py
@@ -6,6 +6,7 @@ import torch
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
+import time
 
 from sam2.build_sam import build_sam2
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
@@ -48,8 +49,9 @@ class bucket:
 class initialize:
     def __init__():
         print('initializing...')
-        sam2_checkpoint = "./checkpoints/sam2_hiera_tiny.pt"
-        model_cfg = "sam2_hiera_t.yaml"
+        # sam2_hiera_b_+ for sam2_hiera_base_plus.pt
+        sam2_checkpoint = "./checkpoints/sam2_hiera_base_plus.pt"
+        model_cfg = "sam2_hiera_b+.yaml"
 
 
         print('building sam')
@@ -75,10 +77,11 @@ class initialize:
         bucket.initialized = True
         
         print('mask generator is generated')
+        print('initialized.\n')
 
 class main:
     def put_anns(anns, img, borders=True):
-        print('Entered annotation generator')
+        a_it=time.perf_counter()
         if len(anns) == 0:
             return
         
@@ -110,7 +113,7 @@ class main:
 
         # Convert float32 image back to 8-bit format for display
         img = (img * 255).astype(np.uint8)
-        
+        print(f'a_dt is {time.perf_counter()-a_it}s')
         return img
 
     def inference(img_path):
@@ -118,39 +121,22 @@ class main:
             print('generator not initialized. Make sure to Initialize!')
             initialize.__init__()
         
+        print('entered inference')
         img = Image.open(img_path)
         img = img.resize((1024,1024))
         img = np.array(img.convert("RGB"))
 
-        masks = bucket.mask_generator.generate(img)
-        
-        print(len(masks))
-        print(masks[0].keys())
+        t = time.perf_counter()
 
         masks2 = bucket.mask_generator_2.generate(img)
         
+        print(f'i_dt is {time.perf_counter() - t}s')
+
         return masks2, img
-
-# example usage
-# image_path = '/home/cfh/Desktop/ClawForHumanity/Sample-Images/Original'
-# output_path = '/home/cfh/Desktop/ClawForHumanity/Sample-Images/SAM2'
-
-# # initialize
-# initialize.__init__()
-
-# # inference
-# masks, img = main.inference(image_path)
-# out = main.put_anns(masks, img)
-
-# # write image
-# cv2.imwrite(output_path, out)
-
 
 
 
 initialize.__init__()
-
-if not bucket.initialized: initialize.__init__()
 
 
 # inference all the files within samples folder
@@ -164,10 +150,62 @@ for file_name in os.listdir(samples_path):
     masks, img = main.inference(file_path)
     out = main.put_anns(masks, img)
 
-    output_path = f'/home/cfh/Desktop/ClawForHumanity/Sample-Images/SAM2/{i}.jpg'
+    output_path = f'/home/cfh/Desktop/ClawForHumanity/Sample-Images/SAM2/hiera_base_+/{i}.jpg'
 
 
 
     Image.fromarray(out).save(output_path)
 
     i += 1
+
+
+# i_dt = inference dt & a_dt = annotation dt
+# all images has been compressed to 1024,1024 for lowering vram usage
+
+# hiera-large
+# dt is 35.07073205300003s
+# dt is 36.02778265799998s
+# dt is 33.807334415000014s
+# dt is 34.258599558000014s
+# dt is 33.17206638400012s
+# dt is 31.748574888999883s
+# dt is 33.91574639500004s
+# dt is 34.58468069100013s
+# dt is 33.333218749000025s
+# dt is 32.170059461000164s
+
+# hiera-tiny
+# dt is 35.13341342000058s
+# dt is 32.64092135499959s
+# dt is 32.32062802699966s
+# dt is 33.11702046699975s
+# dt is 33.283237670000744s
+# dt is 32.616562632999376s
+# dt is 33.37883453999984s
+# dt is 33.03903981399981s
+# dt is 32.811151501000495s
+# dt is 32.12016739400042s
+
+# hiera-small 
+# i_dt is 35.65913728099986s
+# i_dt is 35.24803240100027s
+# i_dt is 33.7598168909999s
+# i_dt is 34.304170647999854s
+# i_dt is 33.931696245999774s
+# i_dt is 32.3495373400001s
+# i_dt is 33.45447230099944s
+# i_dt is 33.663282902999526s
+# i_dt is 33.9859660989996s
+# i_dt is 34.715470559000096s
+
+# hiera-base-plus
+# i_dt is 35.188555478000126s
+# i_dt is 35.50063874100124s
+# i_dt is 36.51252420200035s
+# i_dt is 36.4709503189988s
+# i_dt is 34.79876738500025s
+# i_dt is 34.087960912998824s
+# i_dt is 35.12954371299929s
+# i_dt is 33.797845246999714s
+# i_dt is 33.72583334800038s
+# i_dt is 35.47843969200039s
